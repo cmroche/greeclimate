20fa6396ff4d6c34f6ff35671599e3eef7246dee
Clifford Roche <clifford.roche@gmail.com>
2020-08-10 21:30:34 -0400

    Refactor tests towards pytest

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,28 +1,27 @@
-import unittest
+import pytest
 
 from greeclimate.discovery import Discovery
 from greeclimate.device import DeviceInfo
 from unittest.mock import patch
 
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.search_devices")
+async def test_discover_device(mock_search_devices):
+    mock_search_devices.return_value = [
+        ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1")
+    ]
 
-class GreeClimateTestCase(unittest.IsolatedAsyncioTestCase):
+    devices = await Discovery.search_devices()
 
-    @patch("greeclimate.network_helper.search_devices")
-    async def testShouldListDevicesWhenSearched(self, mock_search_devices):
-        mock_search_devices.return_value = [
-            ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1")
-        ]
+    assert devices is not None
+    assert len(devices) > 0
 
-        devices = await Discovery.search_devices()
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.search_devices")
+async def test_discover_no_devices(mock_search_devices):
+    mock_search_devices.return_value = []
 
-        self.assertIsNotNone(devices)
-        self.assertGreater(len(devices), 0, "No devices were discovered")
+    devices = await Discovery.search_devices()
 
-    @patch("greeclimate.network_helper.search_devices")
-    async def testShouldReturnEmptyListDevicesWhenNoDevicesFound(self, mock_search_devices):
-        mock_search_devices.return_value = []
-
-        devices = await Discovery.search_devices()
-
-        self.assertIsNotNone(devices)
-        self.assertEqual(len(devices), 0)
+    assert devices is not None
+    assert len(devices) == 0
--- tests/test_gree_device.py
+++ tests/test_gree_device.py
@@ -1,6 +1,6 @@
 import enum
+import pytest
 import socket
-import unittest
 
 from greeclimate.discovery import Discovery
 from greeclimate.device import Device, DeviceInfo
@@ -11,237 +11,241 @@ from unittest.mock import patch
 class FakeProps(enum.Enum):
     FAKE = "fake"
 
-class GreeDeviceTestCase(unittest.IsolatedAsyncioTestCase):
-    @patch("greeclimate.network_helper.search_devices")
-    @patch("greeclimate.network_helper.bind_device")
-    async def test_get_device_info(self, mock_bind, mock_search):
-        # DeviceInfo("192.168.1.29", 7000, "f4911e7aca59", "1e7aca59")
+def get_mock_state():
+    return {
+        "Pow": 1,
+        "Mod": 3,
+        "SetTem": 25,
+        "TemUn": 0,
+        "WdSpd": 0,
+        "Air": 0,
+        "Blo": 0,
+        "Health": 0,
+        "SwhSlp": 0,
+        "Lig": 1,
+        "SwingLfRig": 1,
+        "SwUpDn": 1,
+        "Quiet": 0,
+        "Tur": 0,
+        "StHt": 0,
+        "SvSt": 0,
+        "TemRec": 0,
+        "HeatCoolType": 0,
+    }
 
-        mock_info = ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1", "MockBrand", "MockModel", "0.0.1-fake")
-        mock_search.return_value = [mock_info]
-        mock_bind.return_value = "St8Vw1Yz4Bc7Ef0H"
+def get_mock_state_off():
+    return {
+        "Pow": 0,
+        "Mod": 0,
+        "SetTem": 0,
+        "TemUn": 0,
+        "WdSpd": 0,
+        "Air": 0,
+        "Blo": 0,
+        "Health": 0,
+        "SwhSlp": 0,
+        "Lig": 0,
+        "SwingLfRig": 0,
+        "SwUpDn": 0,
+        "Quiet": 0,
+        "Tur": 0,
+        "StHt": 0,
+        "SvSt": 0,
+        "TemRec": 0,
+        "HeatCoolType": 0,
+    }
 
-        """ The only way to get the key through binding is by scanning first
-        """
-        devices = await Discovery.search_devices()
-        device = Device(devices[0])
+def get_mock_state_on():
+    return {
+        "Pow": 1,
+        "Mod": 1,
+        "SetTem": 1,
+        "TemUn": 1,
+        "WdSpd": 1,
+        "Air": 1,
+        "Blo": 1,
+        "Health": 1,
+        "SwhSlp": 1,
+        "Lig": 1,
+        "SwingLfRig": 1,
+        "SwUpDn": 1,
+        "Quiet": 1,
+        "Tur": 1,
+        "StHt": 1,
+        "SvSt": 1,
+        "TemRec": 0,
+        "HeatCoolType": 0,
+    }
+
+async def generate_device_mock_async():
+    d = Device(
+        ("192.168.1.29", 7000, "f4911e7aca59", "1e7aca59")
+    )
+    await d.bind(key="St8Vw1Yz4Bc7Ef0H")
+    return d
+
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.search_devices")
+@patch("greeclimate.network_helper.bind_device")
+async def test_get_device_info(mock_bind, mock_search):
+    # DeviceInfo("192.168.1.29", 7000, "f4911e7aca59", "1e7aca59")
+
+    mock_info = ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1", "MockBrand", "MockModel", "0.0.1-fake")
+    mock_search.return_value = [mock_info]
+    mock_bind.return_value = "St8Vw1Yz4Bc7Ef0H"
+
+    """ The only way to get the key through binding is by scanning first
+    """
+    devices = await Discovery.search_devices()
+    device = Device(devices[0])
+    await device.bind()
+
+    assert devices is not None
+    assert len(devices) == 1
+    
+    assert devices[0].ip ==  mock_info[0]
+    assert devices[0].port ==  mock_info[1]
+    assert devices[0].mac ==  mock_info[2]
+    assert devices[0].name ==  mock_info[3]
+    assert devices[0].brand ==  mock_info[4]
+    assert devices[0].model ==  mock_info[5]
+    assert devices[0].version ==  mock_info[6]
+
+    assert device is not None
+    assert device.device_key ==  "St8Vw1Yz4Bc7Ef0H"
+
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.search_devices")
+@patch("greeclimate.network_helper.bind_device")
+async def test_get_device_key_timeout(mock_bind, mock_search):
+
+    mock_search.return_value = [
+        ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1")
+    ]
+    mock_bind.side_effect = socket.timeout
+
+    """ The only way to get the key through binding is by scanning first
+    """
+    devices = await Discovery.search_devices()
+    device = Device(devices[0])
+
+    with pytest.raises(DeviceNotBoundError):
         await device.bind()
 
-        self.assertIsNotNone(devices)
-        self.assertEqual(len(devices), 1)
-        self.assertEqual(devices[0].ip, mock_info[0])
-        self.assertEqual(devices[0].port, mock_info[1])
-        self.assertEqual(devices[0].mac, mock_info[2])
-        self.assertEqual(devices[0].name, mock_info[3])
-        self.assertEqual(devices[0].brand, mock_info[4])
-        self.assertEqual(devices[0].model, mock_info[5])
-        self.assertEqual(devices[0].version, mock_info[6])
+    assert device is not None
+    assert device.device_key is None
 
-        self.assertIsNotNone(device)
-        self.assertEqual(device.device_key, "St8Vw1Yz4Bc7Ef0H")
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.request_state")
+async def test_update_properties(mock_request):
+    mock_request.return_value = get_mock_state()
+    device = await generate_device_mock_async()
 
-    @patch("greeclimate.network_helper.search_devices")
-    @patch("greeclimate.network_helper.bind_device")
-    async def test_get_device_key_timeout(self, mock_bind, mock_search):
+    for p in Props:
+        assert device.get_property(p) is None
 
-        mock_search.return_value = [
-            ("1.1.1.0", "7000", "aabbcc001122", "MockDevice1")
-        ]
-        mock_bind.side_effect = socket.timeout
+    await device.update_state()
 
-        """ The only way to get the key through binding is by scanning first
-        """
-        devices = await Discovery.search_devices()
-        device = Device(devices[0])
+    for p in Props:
+        assert device.get_property(p) is not None
+        assert device.get_property(p) == get_mock_state()[p.value]
 
-        with self.assertRaises(DeviceNotBoundError):
-            await device.bind()
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.request_state", side_effect=socket.timeout)
+async def test_update_properties_timeout(mock_request):
+    mock_request.return_value = get_mock_state()
+    device = await generate_device_mock_async()
 
-        self.assertIsNotNone(device)
-        self.assertIsNone(device.device_key)
+    for p in Props:
+        assert device.get_property(p) is None
 
+    with pytest.raises(DeviceTimeoutError):
+        await device.update_state()
 
-class GreeDeviceStateTestCase(unittest.IsolatedAsyncioTestCase):
-    async def asyncSetUp(self):
-        self._device = Device(
-            ("192.168.1.29", 7000, "f4911e7aca59", "1e7aca59")
-        )
-        await self._device.bind(key="St8Vw1Yz4Bc7Ef0H")
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.send_state")
+async def test_set_properties(mock_request):
+    device = await generate_device_mock_async()
 
-    def getMockState(self):
-        return {
-            "Pow": 1,
-            "Mod": 3,
-            "SetTem": 25,
-            "TemUn": 0,
-            "WdSpd": 0,
-            "Air": 0,
-            "Blo": 0,
-            "Health": 0,
-            "SwhSlp": 0,
-            "Lig": 1,
-            "SwingLfRig": 1,
-            "SwUpDn": 1,
-            "Quiet": 0,
-            "Tur": 0,
-            "StHt": 0,
-            "SvSt": 0,
-            "TemRec": 0,
-            "HeatCoolType": 0,
-        }
+    for p in Props:
+        assert device.get_property(p) is None
 
-    def getMockStateOff(self):
-        return {
-            "Pow": 0,
-            "Mod": 0,
-            "SetTem": 0,
-            "TemUn": 0,
-            "WdSpd": 0,
-            "Air": 0,
-            "Blo": 0,
-            "Health": 0,
-            "SwhSlp": 0,
-            "Lig": 0,
-            "SwingLfRig": 0,
-            "SwUpDn": 0,
-            "Quiet": 0,
-            "Tur": 0,
-            "StHt": 0,
-            "SvSt": 0,
-            "TemRec": 0,
-            "HeatCoolType": 0,
-        }
+    device.power = True
+    device.mode = 1
+    device.target_temperature = 1
+    device.temperature_units = 1
+    device.fan_speed = 1
+    device.fresh_air = True
+    device.xfan = True
+    device.anion = True
+    device.sleep = True
+    device.light = True
+    device.horizontal_swing = 1
+    device.vertical_swing = 1
+    device.quiet = True
+    device.turbo = True
+    device.steady_heat = True
+    device.power_save = True
 
-    def getMockStateOn(self):
-        return {
-            "Pow": 1,
-            "Mod": 1,
-            "SetTem": 1,
-            "TemUn": 1,
-            "WdSpd": 1,
-            "Air": 1,
-            "Blo": 1,
-            "Health": 1,
-            "SwhSlp": 1,
-            "Lig": 1,
-            "SwingLfRig": 1,
-            "SwUpDn": 1,
-            "Quiet": 1,
-            "Tur": 1,
-            "StHt": 1,
-            "SvSt": 1,
-            "TemRec": 0,
-            "HeatCoolType": 0,
-        }
+    await device.push_state_update()
 
-    @patch("greeclimate.network_helper.request_state")
-    async def test_update_properties(self, mock_request):
-        mock_request.return_value = self.getMockState()
+    mock_request.assert_called_once()
 
-        for p in Props:
-            self.assertIsNone(self._device.get_property(p))
+    for p in Props:
+        if p not in (Props.TEMP_BIT, Props.UNKNOWN_HEATCOOLTYPE):
+            assert device.get_property(p) is not None
+            assert device.get_property(p) == get_mock_state_on()[p.value]
 
-        await self._device.update_state()
+@pytest.mark.asyncio
+@patch("greeclimate.network_helper.send_state", side_effect=socket.timeout)
+async def test_set_properties_timeout(mock_request):
+    device = await generate_device_mock_async()
 
-        for p in Props:
-            self.assertIsNotNone(
-                self._device.get_property(p), f"Property {p} was unexpectedly None"
-            )
-            self.assertEqual(self._device.get_property(p), self.getMockState()[p.value])
+    for p in Props:
+        assert device.get_property(p) is None
 
-    @patch("greeclimate.network_helper.request_state", side_effect=socket.timeout)
-    async def test_update_properties_timeout(self, mock_request):
-        mock_request.return_value = self.getMockState()
+    device.power = True
+    device.mode = 1
+    device.target_temperature = 1
+    device.temperature_units = 1
+    device.fan_speed = 1
+    device.fresh_air = True
+    device.xfan = True
+    device.anion = True
+    device.sleep = True
+    device.light = True
+    device.horizontal_swing = 1
+    device.vertical_swing = 1
+    device.quiet = True
+    device.turbo = True
+    device.steady_heat = True
+    device.power_save = True
 
-        for p in Props:
-            self.assertIsNone(self._device.get_property(p))
+    with pytest.raises(DeviceTimeoutError):
+        await device.push_state_update()
 
-        with self.assertRaises(DeviceTimeoutError):
-            await self._device.update_state()
+@pytest.mark.asyncio
+async def test_uninitialized_properties():
+    device = await generate_device_mock_async()
 
-    @patch("greeclimate.network_helper.send_state")
-    async def test_set_properties(self, mock_request):
+    for p in Props:
+        assert device.get_property(p) is None
 
-        for p in Props:
-            self.assertIsNone(self._device.get_property(p))
-
-        self._device.power = True
-        self._device.mode = 1
-        self._device.target_temperature = 1
-        self._device.temperature_units = 1
-        self._device.fan_speed = 1
-        self._device.fresh_air = True
-        self._device.xfan = True
-        self._device.anion = True
-        self._device.sleep = True
-        self._device.light = True
-        self._device.horizontal_swing = 1
-        self._device.vertical_swing = 1
-        self._device.quiet = True
-        self._device.turbo = True
-        self._device.steady_heat = True
-        self._device.power_save = True
-
-        await self._device.push_state_update()
-
-        mock_request.assert_called_once()
-
-        for p in Props:
-            if p not in (Props.TEMP_BIT, Props.UNKNOWN_HEATCOOLTYPE):
-                self.assertIsNotNone(
-                    self._device.get_property(p), f"Property {p} was unexpectedly None"
-                )
-                self.assertEqual(
-                    self._device.get_property(p), self.getMockStateOn()[p.value]
-                )
-
-    @patch("greeclimate.network_helper.send_state", side_effect=socket.timeout)
-    async def test_set_properties_timeout(self, mock_request):
-
-        for p in Props:
-            self.assertIsNone(self._device.get_property(p))
-
-        self._device.power = True
-        self._device.mode = 1
-        self._device.target_temperature = 1
-        self._device.temperature_units = 1
-        self._device.fan_speed = 1
-        self._device.fresh_air = True
-        self._device.xfan = True
-        self._device.anion = True
-        self._device.sleep = True
-        self._device.light = True
-        self._device.horizontal_swing = 1
-        self._device.vertical_swing = 1
-        self._device.quiet = True
-        self._device.turbo = True
-        self._device.steady_heat = True
-        self._device.power_save = True
-
-        with self.assertRaises(DeviceTimeoutError):
-            await self._device.push_state_update()
-
-
-    async def test_uninitialized_properties(self):
-
-        for p in Props:
-            self.assertIsNone(self._device.get_property(p))
-
-        self.assertFalse(self._device.power)
-        self.assertIsNone(self._device.mode)
-        self.assertIsNone(self._device.target_temperature)
-        self.assertIsNone(self._device.temperature_units)
-        self.assertIsNone(self._device.fan_speed)
-        self.assertFalse(self._device.fresh_air)
-        self.assertFalse(self._device.xfan)
-        self.assertFalse(self._device.anion)
-        self.assertFalse(self._device.sleep)
-        self.assertFalse(self._device.light)
-        self.assertIsNone(self._device.horizontal_swing)
-        self.assertIsNone(self._device.vertical_swing)
-        self.assertFalse(self._device.quiet)
-        self.assertFalse(self._device.turbo)
-        self.assertFalse(self._device.steady_heat)
-        self.assertFalse(self._device.power_save)
+    assert not device.power
+    assert device.mode is None
+    assert device.target_temperature is None
+    assert device.temperature_units is None
+    assert device.fan_speed is None
+    assert not device.fresh_air
+    assert not device.xfan
+    assert not device.anion
+    assert not device.sleep
+    assert not device.light
+    assert device.horizontal_swing is None
+    assert device.vertical_swing is None
+    assert not device.quiet
+    assert not device.turbo
+    assert not device.steady_heat
+    assert not device.power_save
 
 
