99da00bad1f6d3d10b30c541cdf3c6be07caea87
Clifford Roche <clifford.roche@gmail.com>
2020-11-13 20:12:40 -0500

    Emulator supports "cmd" and "state"

--- emulator.py
+++ emulator.py
@@ -67,6 +67,76 @@ def scan_response(addr):
     send_device_data(addr, resp, GENERIC_KEY)
 
 
+state_cols = [
+    "Pow",
+    "Mod",
+    "SetTem",
+    "TemUn",
+    "TemRec",
+    "WdSpd",
+    "Air",
+    "Blo",
+    "Health",
+    "SwhSlp",
+    "Lig",
+    "SwingLfRig",
+    "SwUpDn",
+    "Quiet",
+    "Tur",
+    "StHt",
+    "SvSt",
+    "HeatCoolType",
+]
+state_dat = [1, 4, 23, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0]
+
+
+def cmd_response(addr, opt, p):
+    for j in range(len(opt)):
+        o = opt[j]
+        i = state_cols.index(o)
+        state_dat[i] = p[j]
+
+    resp = {
+        "t": "pack",
+        "i": 0,
+        "uid": 0,
+        "cid": device_id,
+        "tcid": "",
+        "pack": {
+            "t": "res",
+            "mac": device_id,
+            "r": 200,
+            "opt": opt,
+            "p": p,
+            "val": p,
+        },
+    }
+    send_device_data(addr, resp, DEVICE_KEY)
+
+
+def status_response(addr, opt):
+    r = []
+    for o in opt:
+        i = state_cols.index(o)
+        r.append(state_dat[i])
+
+    resp = {
+        "t": "pack",
+        "i": 0,
+        "uid": 0,
+        "cid": device_id,
+        "tcid": "",
+        "pack": {
+            "t": "dat",
+            "mac": device_id,
+            "r": 200,
+            "cols": state_cols,
+            "dat": r,
+        },
+    }
+    send_device_data(addr, resp, DEVICE_KEY)
+
+
 def bind_response(addr):
     resp = {
         "t": "pack",
@@ -94,7 +164,7 @@ while True:
         scan_response(addr)
     elif command == "pack":
         # Decrypt the incoming request
-        key = GENERIC_KEY if p.get("cid") == "app" else DEVICE_KEY
+        key = GENERIC_KEY if p.get("i") == 1 else DEVICE_KEY
         cipher = aes(key, 1)
         decoded = ubinascii.a2b_base64(p.get("pack"))
         decrypted = cipher.decrypt(decoded).decode()
@@ -105,5 +175,9 @@ while True:
         command = data.get("t")
         if command == "bind":
             bind_response(addr)
+        elif command == "cmd":
+            cmd_response(addr, data.get("opt"), data.get("p"))
+        elif command == "status":
+            status_response(addr, data.get("cols"))
 
     time.sleep_ms(500)
