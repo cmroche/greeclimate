ea3163ead93adea39fd760ec3ab1a5d8a190e1f0
Clifford Roche <clifford.roche@gmail.com>
2021-10-28 15:34:13 -0400

    Prepare 0.12.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="greeclimate",
-    version="0.12.2",
+    version="0.12.3",
     python_requires=">=3.6",
     install_requires=requirements,
     author="Clifford Roche",
